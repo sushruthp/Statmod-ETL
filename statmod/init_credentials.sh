f78c251628c1da034ae17a9e62eda61a030e3a82
Sushruth Puttaswamy <sushruth.puttaswamy@nor1.com>
2011-12-27 16:30:56 -0800

    Removed all usernames/passwords

--- statmod/init_credentials.sh
+++ statmod/init_credentials.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-export statmod_uname=ahines
-export statmod_pwd=luckyEGuy!
-export statmod_db_name=statmod
+export statmod_uname=AAAA
+export statmod_pwd=AAAA
+export statmod_db_name=AAAA
 
-export analytics_uname=rmodel
-export analytics_pwd=ST@tist1cs
-export analytics_db_host=10.40.19.2
-export analytics_db_name=analytics
+export analytics_uname=BBBB
+export analytics_pwd=BBBB
+export analytics_db_host=BBBB
+export analytics_db_name=BBBB
 
-export sas_db_host=10.36.81.2
-export sas_db_uname=stats
-export sas_db_pwd=St@tus1
-export sas_db_name=nor1_upg_db1_dal_us
+export sas_db_host=CCCC
+export sas_db_uname=CCCC
+export sas_db_pwd=CCCC
+export sas_db_name=CCCC
